7a653b28a6da44951438d77d8abc419db6f61e9e
nirajsingh0184 <nirajsingh0185@gmail.com>
2021-08-28 16:20:16 +0530

    updated create

--- controller/todo.js
+++ controller/todo.js
@@ -31,11 +31,10 @@ const arr = {
 
     getTask: (req, res) => {
         const { id } = req.params;
-        let element=[];
+        let element = [];
         task.forEach(tasks => {
 
-            if(id==tasks.id)
-            {
+            if (id == tasks.id) {
                 element.push(tasks);
             }
         })
@@ -44,74 +43,83 @@ const arr = {
 
     createTask: (req, res) => {
 
-        const {body}=req;
-        for(let i=0;i<task.length;i++)
+        const { body } = req;
+        const { id, ...rest } = body;
+        console.log(id);
+        if (id) 
         {
-            if(task[i].id1==body.id)
+            for (let i = 0; i < task.length; i++) 
             {
-               return res.send(
+                if (task[i].id == id) 
                 {
-                    status:401,
-                    message:"task Already exist"
+                    return res.send(
+                        {
+                            status: 400,
+                            message: "task Already exist"
+                        }
+                    );
                 }
-               );
             }
+            task.push(body);
+           return res.send(
+                {
+                    status: 200,
+                    message: "success"
+                })
         }
-        
-        task.push(body);
+        else 
+        {
+            const uniqueId = Math.floor(Math.random() * 1000000);
+            const newTask = { ...body, id: uniqueId };
+        task.push(newTask);
         res.send(
             {
-                status:200,
-                message:"success"
+                status: 200,
+                message: "success"
             }
         )
+        }
     },
 
-deleteTask:(req,res)=>
-{
-    const { id } = req.params;
-    for(let i=0;i<task.length;i++)
-    {
-        if(task[i].id==id)
-        {
-            task.splice(i,1);
+    deleteTask: (req, res) => {
+        const { id } = req.params;
+        for (let i = 0; i < task.length; i++) {
+            if (task[i].id == id) {
+                task.splice(i, 1);
+            }
         }
-    }
-   console.log(task);
-   
-   res.send(task);
-},
+        console.log(task);
 
-updateTask:(req,res)=>
-{
-    const { id } = req.params;
-    const { body }=req;
-    const {id1,...rest}=body;
-    console.log(rest);
-    
-    for(let i=0;i<task.length;i++)
-    {
-        if(task[i].id1==id)
-        {
-            task[i]={...task[i],...rest};
+        res.send(task);
+    },
+
+    updateTask: (req, res) => {
+        const { id } = req.params;
+        const { body } = req;
+        const { id1, ...rest } = body;
+        console.log(rest);
+
+        for (let i = 0; i < task.length; i++) {
+            if (task[i].id1 == id) {
+                task[i] = { ...task[i], ...rest };
+            }
         }
+        res.send(task);
     }
-  res.send(task);
-}
 
-// fun6:return new Promise(resolve,reject) => {
-//     // //connection.query('SELECT * FROM users', (err, rows, fields) => {
-//     //   if (err)
-//     //     throw err;
-//     //   else
-//     //     res.send(rows);
+    // fun6:return new Promise(resolve,reject) => {
+    //     // //connection.query('SELECT * FROM users', (err, rows, fields) => {
+    //     //   if (err)
+    //     //     throw err;
+    //     //   else
+    //     //     res.send(rows);
 
 
-//     })
+    //     })
 
     // new Promise(function(resolve, reject) {
     //     // do a thing, possibly async, then…
-      
+
     //     if (a==7) {
     //       resolve("Stuff worked!");
     //     }
--- model/todo.js
+++ model/todo.js
@@ -1,12 +1,12 @@
 let task=[
     {
-        id1:"1",
+        id:"1",
         name:"task 1",
         status:"false",
         description:"jgjgjgj"
     },
     {
-        id1:"2",
+        id:"2",
         name:"task 2",
         status:"false",
         description:"jgjgjgj"
